3001d423f26d578d6271cca57cd1d5df4c57dbd9
Demetrio Quiñones <demetriorqe@gmail.com>
2021-10-31 16:00:09 -0400

    changes for deploy

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 
-app.use('/api', routes);
+app.use('/', routes);
 
 app.listen(PORT, () => {
   console.log(`Server listeniong on ${PORT}`);
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,14 +2,13 @@ var express = require('express');
 var router = express.Router();
 const userController = require('../controllers/user.controller');
 const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, '.env') });
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 const env = process.env;
 
 // middleware that is specific to this router
 router.use(function authenticate(req, res, next) {
   const apiKey = req.query.api_key;
-  console.log(apiKey);
-  if (!apiKey || apiKey !== env.ApiKey) {
+  if ( apiKey !== env.ApiKey) {
     res.status(401).send({ Message: 'Not authorized' });
   } else {
     next();
