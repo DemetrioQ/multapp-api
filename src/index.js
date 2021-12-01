601988daec2ae40665c3d3d5e0478e22d17ce6a0
Demetrio Quiñones <demetriorqe@gmail.com>
2021-12-01 18:25:17 -0400

    Added new routh for user verification

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ var userRoutes = require('./routes/user.routes');
 var penaltyRoutes = require('./routes/penalty.routes');
 var personRoutes = require('./routes/person.routes');
 var provinceRoutes = require('./routes/province.routes');
+var authRoutes = require('./routes/auth.routes');
 const path = require('path');
 var cors = require('cors');
 require('./config/db.relations');
@@ -13,12 +14,16 @@ require('dotenv').config({ path: path.resolve(__dirname, '.env') });
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+app.set('view engine', 'ejs');
 // db.sequelize.sync();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use('/', routes);
+app.use('/auth', authRoutes);
+
+
+app.use('/api', routes);
 app.use('/api/user', userRoutes);
 app.use('/api/penalty', penaltyRoutes);
 app.use('/api/person', personRoutes);
